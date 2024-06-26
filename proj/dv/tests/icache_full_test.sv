3ac94839ffab151c7dc8f180caccf7917eb1120f
Jack Sabine <jack@sabine.ws>
2024-06-26 12:00:12 -0400

    Delete unused tests

--- proj/dv/tests/icache_full_test.sv
+++
@@ -1,7 +0,0 @@
-class icache_full_test extends cache_base_test;
-    `uvm_component_utils(icache_full_test)
-
-    virtual function void set_transaction_type();
-        memory_transaction::type_id::set_type_override(icache_memory_transaction::get_type());
-    endfunction
-endclass
